78abcf17e780a1c68bab0bc44a90b1835ff550a0
chintan <chintan@gmail.com>
2026-01-12 17:30:49 +0530

    'all'

---
+++ Basic/Datatypes.java
@@ -0,0 +1,22 @@
+class Datatypes
+{
+    public static void main(String args[])
+    {
+        byte b = -128;
+        System.out.println(b);
+        short s = 456;
+        int i = 4564;
+        long l = 656565;
+
+        float f = 4546.1234456789f;
+        double d = 4568.123456789;
+
+    System.out.println(f);
+    System.out.println(d);
+
+    char ch = 'A'
+
+    boolean b  = True
+
+    }
+}
---
+++ Basic/Variables.java
@@ -0,0 +1,18 @@
+class Variables
+{
+    public static void main(String args[])
+    {
+        //Rules
+    //     int _id123 = 50;
+
+    //     int a = 10;
+    //     int A = 20;
+    //     System.out.println(A);
+
+    //conventiona//
+    String myFirstName = "Bhagya";
+
+
+    }
+}
+
